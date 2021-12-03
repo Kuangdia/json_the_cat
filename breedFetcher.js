2db416bd01e4615dc3f7dacda2c87de770ed297f
Diana <slorrrr@gmail.com>
2021-12-02 20:29:07 -0500

    refactored using callback with index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,31 @@
 const request = require('request');
+// const fetchBreedDescription = require('./index');
 
-request('https://api.thecatapi.com/v1/breeds/search?q=sib', (err, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
+  const URL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-  if (err) {
-    return console.log("This is an error", err);
-  }
-  // converts JSON body text ("string") into object
-  const data = JSON.parse(body);
+  // original code is async => we need to wrap in async fx
+  // so our fBD fx doesn't finish running before we get to request
+  request(URL, (err, response, body) => {
 
-  // to access first object in an array
-  if (data.length === 0) {
-    console.log("error message?");
-  }
-  console.log(data[0].description);
+    if (err) {
+      // this error from URL request will be passed to index js if (error)
+      return callback(err, null);
+    }
 
+    // converts JSON body text ("string") into object
+    const data = JSON.parse(body);
+  
+    // the wrong breedName will return [] in webbrowser (we can't compare objects)
+    if (data.length === 0) {
+      // this string is passed into first parameter in index.js
+      callback('Breed not found', null);
+    } else {
+      // the description is passed into second parameter in index.js
+      callback(null, data[0].description);
+    }
 
-});
+  });
+};
+
+module.exports = { fetchBreedDescription };
